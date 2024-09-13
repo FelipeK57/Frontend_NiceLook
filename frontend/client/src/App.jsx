5851e725779ecf0cda45394fe34dfebad26e3796
FelipeK57 <166841728+FelipeK57@users.noreply.github.com>
2024-09-13 11:51:33 -0500

    creación de proyecto con tailwind y configuración

---
+++ frontend/client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-100 h-screen w-full grid place-content-center">
+      <h1 className="text-4xl text-zinc-950 font-semibold">
+        Este es el frontend
+      </h1>
+    </div>
+  );
+}
+
+export default App;
