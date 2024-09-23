d7305a6cd2deb487b35f4f8a4c2e19f593200829
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-09-23 15:06:12 -0500

    Actualizar rutas de React Router Dom y agregar página de inicio de sesión de administrador

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,34 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  // Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import AdminLogin from "./pages/auth/admin/AdminLogin.jsx";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <div>Error Page</div>,
+  },
+  {
+    path: "/admin",
+    // element: <Navigate to="./login"/>,
+    children: [
+      {
+        id: "admin-login",
+        path: "login",
+        element: <AdminLogin />,
+      },
+      {
+        id: "admin-dashboard",
+        path: "dashboard",
+        element: <div>Dashboard Page</div>,
+      },
+    ],
   },
 ]);
 
