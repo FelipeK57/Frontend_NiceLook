dc34cbb61632280e9f42493875242e449c6df9df
FelipeK57 <166841728+FelipeK57@users.noreply.github.com>
2024-09-24 13:38:36 -0500

    Agregamos a los componentes el modal de editar servicios, barra de scroll a la lista de servicios y modificación en el ui kit

--- client/src/components/servicesComponents/Categories.jsx
+++ client/src/components/servicesComponents/Categories.jsx
@@ -1,12 +1,17 @@
+import { Button } from "@nextui-org/react";
+
 function Categories() {
   return (
-    <article className="flex flex-col items-center border-2 border-slate-200 rounded-xl py-4 px-2">
+    <article className="flex flex-col gap-4 items-center border-2 border-slate-200 rounded-xl p-4">
       <h1 className="text-xl font-bold">Categorias</h1>
-      <ul className="flex flex-col gap-4 [&>li]:font-semibold items-start p-2 w-full">
-        <li>Barberia</li>
-        <li>SPA de uñas</li>
-        <li>Peluqueria</li>
+      <ul className="flex flex-col gap-4 [&>li]:w-full [&>li]:py-2 [&>li]:font-semibold items-start p-2 w-full">
+        <li className="border-b-2 border-slate-950">Barberia</li>
+        <li className="text-slate-500">SPA de uñas</li>
+        <li className="text-slate-500">Peluqueria</li>
       </ul>
+      <Button className="font-semibold border-2 border-slate-200 rounded-xl bg-transparent">
+        Añadir nueva
+      </Button>
     </article>
   );
 }
