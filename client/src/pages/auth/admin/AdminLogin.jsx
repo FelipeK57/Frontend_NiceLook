96c066ec82688c6af21d3686195bbd031a66ae98
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-09-26 11:59:39 -0500

    Actualizar página de inicio de sesión de administrador

--- client/src/pages/auth/admin/AdminLogin.jsx
+++ client/src/pages/auth/admin/AdminLogin.jsx
@@ -1,7 +1,29 @@
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+
 export default function AdminLogin() {
   return (
     <>
-      <h1 className="m-auto">Login del admin</h1>
+      <main className="h-screen flex items-center">
+        <article className="flex items-center w-[75%] mx-auto h-4/5">
+          <section className="w-full h-full flex justify-center items-center">
+            <h1 className="font-bold text-3xl">
+              Esto es <span className="">NiceLook.</span>
+            </h1>
+          </section>
+          <section className="w-full h-full p-8">
+            <header>
+              <h1 className="font-bold text-3xl">Regístrate</h1>
+            </header>
+            <div>Login oauth google todo</div>
+            <form>
+              <Input type="email" placeholder="Correo electrónico" />
+              <Input type="password" placeholder="Contraseña" />
+              <Button type="submit">Login</Button>
+            </form>
+          </section>
+        </article>
+      </main>
     </>
   );
 }
