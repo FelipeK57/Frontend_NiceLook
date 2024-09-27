c9aaa1344364efca6dc52a752f200c85581d235b
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-09-27 18:43:29 -0500

    añadir shadcn y componente separator

--- client/src/pages/auth/admin/AdminLogin.jsx
+++ client/src/pages/auth/admin/AdminLogin.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
+import { Separator } from "@/components/ui/separator";
 
 export default function AdminLogin() {
   return (
@@ -8,14 +9,19 @@ export default function AdminLogin() {
         <article className="flex items-center w-[75%] mx-auto h-4/5">
           <section className="w-full h-full flex justify-center items-center">
             <h1 className="font-bold text-3xl">
-              Esto es <span className="">NiceLook.</span>
+              Esto es{" "}
+              <span className="font-[Amaranth]">
+                Nice<span className="text-slate-700">Look</span>
+                <span className="text-tulip-tree-400">.</span>
+              </span>
             </h1>
           </section>
-          <section className="w-full h-full p-8">
+          <section className="w-full h-full p-8 flex flex-col gap-5">
             <header>
               <h1 className="font-bold text-3xl">Regístrate</h1>
             </header>
             <div>Login oauth google todo</div>
+            <Separator />
             <form>
               <Input type="email" placeholder="Correo electrónico" />
               <Input type="password" placeholder="Contraseña" />
