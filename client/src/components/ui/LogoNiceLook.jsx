8770b8b3db5ac03d736d807a43bdbe8acfc79606
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-09-28 21:08:53 -0500

    Agregar componente LogoNiceLook para el logotipo personalizado

---
+++ client/src/components/ui/LogoNiceLook.jsx
@@ -0,0 +1,8 @@
+export default function LogoNiceLook() {
+  return (
+    <span className="font-bold font-[Amaranth]">
+      Nice<span className="text-slate-700">Look</span>
+      <span className="text-tulip-tree-400">.</span>
+    </span>
+  );
+}
