59411e904cf877a8ff395051f3281800aee3c4e8
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-09-28 21:09:01 -0500

    Agregar componentes LogoNiceLook, ButtonCustom y BackgroundLogin en AdminLogin

--- client/src/pages/auth/admin/AdminLogin.jsx
+++ client/src/pages/auth/admin/AdminLogin.jsx
@@ -1,31 +1,108 @@
-import { Button } from "@nextui-org/button";
+import { useState } from "react";
+import api from "@/api";
+
+import ButtonCustom from "@/components/global/ButtonCustom";
 import { Input } from "@nextui-org/input";
 import { Separator } from "@/components/ui/separator";
+import BackgroundLogin from "@/components/global/BackgroundLogin";
+import LogoNiceLook from "@/components/ui/LogoNiceLook";
+
+import { GoogleLogin } from "@react-oauth/google";
 
 export default function AdminLogin() {
+  const [formData, setFormData] = useState({
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    setLoading(true);
+    e.preventDefault();
+    console.log("Form data", formData);
+    api.post("/auth/register", formData).then((response) => {
+      console
+        .log(response)
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    });
+  };
+
   return (
     <>
       <main className="h-screen flex items-center">
-        <article className="flex items-center w-[75%] mx-auto h-4/5">
+        <BackgroundLogin />
+        <article className="flex flex-col md:flex-row items-center w-[75%] mx-auto h-4/5">
           <section className="w-full h-full flex justify-center items-center">
-            <h1 className="font-bold text-3xl">
-              Esto es{" "}
-              <span className="font-[Amaranth]">
-                Nice<span className="text-slate-700">Look</span>
-                <span className="text-tulip-tree-400">.</span>
-              </span>
+            <h1 className="font-bold text-4xl">
+              Esto es <LogoNiceLook />
             </h1>
           </section>
           <section className="w-full h-full p-8 flex flex-col gap-5">
             <header>
               <h1 className="font-bold text-3xl">Regístrate</h1>
             </header>
-            <div>Login oauth google todo</div>
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                console.log(credentialResponse);
+              }}
+              onError={() => {
+                console.log("Fallo en el login");
+              }}
+            />
             <Separator />
-            <form>
-              <Input type="email" placeholder="Correo electrónico" />
-              <Input type="password" placeholder="Contraseña" />
-              <Button type="submit">Login</Button>
+            <p>También puedes registrarte con tu correo electrónico.</p>
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col flex-nowrap gap-5"
+            >
+              <Input
+                type="text"
+                placeholder="Nombres"
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+                defaultValue={formData.name}
+              />
+              <Input
+                type="text"
+                placeholder="Apellidos"
+                onChange={(e) =>
+                  setFormData({ ...formData, lastname: e.target.value })
+                }
+                defaultValue={formData.lastname}
+              />
+              <Input
+                type="email"
+                placeholder="Correo electrónico"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                defaultValue={formData.email}
+              />
+              <Input
+                type="password"
+                placeholder="Contraseña"
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+                defaultValue={formData.password}
+              />
+              <ButtonCustom
+                type="submit"
+                name="Entrar"
+                primary
+                isDisabled={
+                  !formData.name || !formData.lastname || !formData.email || !formData.password
+                }
+                isLoading={loading}
+              />
             </form>
           </section>
         </article>
