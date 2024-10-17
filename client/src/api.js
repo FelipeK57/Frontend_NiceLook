dd045bd77d0941e71cd411139038ee3d8820567e
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-10-16 20:14:47 -0500

    Actualizar api.js para comentar el código relacionado con las cookies y el token de acceso

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import Cookies from "js-cookie";
-import { ACCESS_TOKEN } from "../constants";
+// import Cookies from "js-cookie";
+// import { ACCESS_TOKEN } from "../constants";
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:8000",
@@ -9,11 +9,10 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = Cookies.get(ACCESS_TOKEN);
-    if (token) {
-      // config.headers.Authorization = `Bearer ${token}`;
-      config.headers["X-XSRF-TOKEN"] = token;
-    }
+    // const token = Cookies.get(ACCESS_TOKEN);
+    // if (token) {
+    //   config.headers.Authorization = `Bearer ${token}`;
+    // }
 
     return config;
   },
