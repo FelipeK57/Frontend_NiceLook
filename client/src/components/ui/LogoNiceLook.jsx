f1182b03b75cbbc45890e72ac199a6c9b472aa66
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-10-16 20:18:04 -0500

    Actualizar LogoNiceLook para aceptar una clase de estilo personalizada

--- client/src/components/ui/LogoNiceLook.jsx
+++ client/src/components/ui/LogoNiceLook.jsx
@@ -1,6 +1,7 @@
-export default function LogoNiceLook() {
+/* eslint-disable react/prop-types */
+export default function LogoNiceLook({ className }) {
   return (
-    <span className="font-bold font-[Amaranth]">
+    <span className={`font-bold font-[Amaranth] ${className}`}>
       Nice<span className="text-slate-700">Look</span>
       <span className="text-tulip-tree-400">.</span>
     </span>
