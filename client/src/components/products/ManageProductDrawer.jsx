a6ea8c18f05086ca5c9cd680006c95cf22753bac
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-10-19 17:27:56 -0500

    Actualizar componente de menú desplegable: Simplificar componente ManageProductDrawer

--- client/src/components/products/ManageProductDrawer.jsx
+++ client/src/components/products/ManageProductDrawer.jsx
@@ -1,18 +1,14 @@
 /* eslint-disable react/prop-types */
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 
-import { Button } from "@nextui-org/react";
+import ManageProduct from "./ManageProduct";
 
-export default function ManageProductDrawer({ isOpen, setIsOpen, children }) {
+export default function ManageProductDrawer({
+  isOpen,
+  setIsOpen,
+  children,
+  product,
+}) {
   return (
     <>
       <Drawer open={isOpen} onOpenChange={setIsOpen}>
@@ -21,15 +17,7 @@ export default function ManageProductDrawer({ isOpen, setIsOpen, children }) {
           {children}
         </DrawerTrigger>
         <DrawerContent>
-          <DrawerHeader className="text-left">
-            <DrawerTitle>hola tituo</DrawerTitle>
-            <DrawerDescription>hola descrision</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter className="pt-2">
-            <DrawerClose asChild>
-              <Button variant="outline">Cancela</Button>
-            </DrawerClose>
-          </DrawerFooter>
+          <ManageProduct product={product} />
         </DrawerContent>
       </Drawer>
     </>
