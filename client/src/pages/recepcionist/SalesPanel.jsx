43a3ef269048fc068be971406310deb9e3e9322a
Kevin Felipe Bolaños Orozco <166841728+FelipeK57@users.noreply.github.com>
2024-10-25 15:27:57 -0500

    creación modal para crear citas y ajustes para interfaz de ventas de recepcionista

--- client/src/pages/recepcionist/SalesPanel.jsx
+++ client/src/pages/recepcionist/SalesPanel.jsx
@@ -7,6 +7,8 @@ import PaymentProductList from "../../components/finances/PaymentProductsList";
 import { parseDate } from "@internationalized/date";
 import axios from "axios";
 import Cookies from "js-cookie";
+import ButtonCustom from "../../components/global/ButtonCustom";
+import ModalCreateAppointment from "../../components/sales/ModalCreateAppointment";
 const items = ["Servicios", "Productos"];
 
 function SalesPanel() {
@@ -17,63 +19,68 @@ function SalesPanel() {
   const [date, setDate] = useState(
     parseDate(`${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`)
   );
-  const [earningsEstablishment, setEarningsEstablishment] = useState(0);
-  const [earningsArtist, setEarningsArtist] = useState(0);
+  // const [earningsEstablishment, setEarningsEstablishment] = useState(0);
+  // const [earningsArtist, setEarningsArtist] = useState(0);
   const [module, setModule] = useState("Servicios");
   const [products, setProducts] = useState([]);
   const [services, setServices] = useState([]);
-  const [earningsProducts, setEarningsProducts] = useState(0);
-  const columnsProducts = ["Precio", "Cantidad", "Metodo de pago", "Productos"];
-  const columnsServices = ["Precio", "Ganancia", "Profesional", "Servicios"];
-  const establishmentId = Cookies.get("establishmentId");
+  // const [earningsProducts, setEarningsProducts] = useState(0);
+  // const columnsProducts = ["Precio", "Cantidad", "Metodo de pago", "Productos"];
+  // const columnsServices = ["Precio", "Ganancia", "Profesional", "Servicios"];
+  // const establishmentId = Cookies.get("establishmentId");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const day = date.toDate().getDate();
-      const month = date.toDate().getMonth() + 1;
-      const year = date.toDate().getFullYear();
-      const response = await axios.get(
-        `http://localhost:8000/establisment/get-filter-payments-service/${establishmentId}/`,
-        {
-          params: {
-            day: day,
-            month: month,
-            year: year,
-          },
-        }
-      );
-      console.log(response.data);
-      setEarningsArtist(response.data.ganancia_employee);
-      setEarningsEstablishment(response.data.ganancia_establecimiento);
-      setServices(response.data.appointments_services);
-    };
-    fetchData();
-  }, [date]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const day = date.toDate().getDate();
+  //     const month = date.toDate().getMonth() + 1;
+  //     const year = date.toDate().getFullYear();
+  //     const response = await axios.get(
+  //       `http://localhost:8000/establisment/get-filter-payments-service/${establishmentId}/`,
+  //       {
+  //         params: {
+  //           day: day,
+  //           month: month,
+  //           year: year,
+  //         },
+  //       }
+  //     );
+  //     console.log(response.data);
+  //     setEarningsArtist(response.data.ganancia_employee);
+  //     setEarningsEstablishment(response.data.ganancia_establecimiento);
+  //     setServices(response.data.appointments_services);
+  //   };
+  //   fetchData();
+  // }, [date]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const day = date.toDate().getDate();
-      const month = date.toDate().getMonth() + 1;
-      const year = date.toDate().getFullYear();
-      const response = await axios.get(
-        "http://localhost:8000/establisment/get-filter-payments-product/1/",
-        {
-          params: {
-            day: day,
-            month: month,
-            year: year,
-          },
-        }
-      );
-      setEarningsProducts(response.data.ganancia_establecimiento);
-      console.log(response.data.ganancia_establecimiento);
-      setProducts(response.data.product_payments);
-      console.log(response.data.product_payments);
-    };
-    fetchData();
-  }, [date]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const day = date.toDate().getDate();
+  //     const month = date.toDate().getMonth() + 1;
+  //     const year = date.toDate().getFullYear();
+  //     const response = await axios.get(
+  //       "http://localhost:8000/establisment/get-filter-payments-product/1/",
+  //       {
+  //         params: {
+  //           day: day,
+  //           month: month,
+  //           year: year,
+  //         },
+  //       }
+  //     );
+  //     setEarningsProducts(response.data.ganancia_establecimiento);
+  //     console.log(response.data.ganancia_establecimiento);
+  //     setProducts(response.data.product_payments);
+  //     console.log(response.data.product_payments);
+  //   };
+  //   fetchData();
+  // }, [date]);
+  const [isModalNewServiceOpen, setIsModalNewServiceOpen] = useState(false);
+
+  const handleOpen = () => setIsModalNewServiceOpen(true);
+  const handleClose = () => setIsModalNewServiceOpen(false);
+
   return (
-    <main className="h-screen flex flex-col py-6 gap-4 px-10">
+    <main className="h-dvh grid grid-rows-[auto_1fr_auto] py-6 gap-4 px-10">
       <header className="flex items-center justify-between">
         <h1 className="text-4xl font-bold text-slate-950">Ventas del día</h1>
         <Select
@@ -102,15 +109,34 @@ function SalesPanel() {
         />
       </header>
       <section className="flex flex-col gap-2">
-        <div className="flex gap-10 items-end">
-          <h2 className="text-2xl font-bold text-slate-950">Resumen</h2>
-          <p className="text-2xl font-bold text-slate-950">
-            {module === "Servicios"
-              ? `Cantidad: ${services.length}`
-              : `Cantidad: ${products.length}`}
-          </p>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-row gap-6">
+            <h2 className="text-xl font-semibold text-slate-950">Resumen</h2>
+            <p className="text-xl font-semibold text-slate-950">
+              {module === "Servicios"
+                ? `Cantidad: ${services.length}`
+                : `Cantidad: ${products.length}`}
+            </p>
+          </div>
+          {module === "Servicios" ? (
+            <>
+              <ButtonCustom name={"Crear cita"} primary action={handleOpen} />
+              <ModalCreateAppointment
+                isOpen={isModalNewServiceOpen}
+                onClose={handleClose}
+              />
+            </>
+          ) : (
+            <ButtonCustom
+              name={"Crear venta"}
+              primary
+              action={() => {
+                alert("Crear una venta");
+              }}
+            />
+          )}
         </div>
-        <div>
+        {/* {<div>
           {module === "Servicios" ? (
             <>
               {services.length === 0 ? (
@@ -156,8 +182,11 @@ function SalesPanel() {
               )}
             </>
           )}
-        </div>
+        </div>} */}
       </section>
+      <footer className="flex gap-10 items-end">
+        <p className="font-semibold text-2xl">Ganancias totales del día: </p>
+      </footer>
     </main>
   );
 }
