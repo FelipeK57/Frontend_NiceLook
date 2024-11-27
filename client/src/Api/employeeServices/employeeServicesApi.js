7b75f39f776c7368d2dfd540f61110decdfddcf8
Camilo Gonzalez <milogonza_01@hotmail.com>
2024-11-26 23:10:37 -0500

    Ver y crear review

--- client/src/Api/employeeServices/employeeServicesApi.js
+++ client/src/Api/employeeServices/employeeServicesApi.js
@@ -1,17 +1,28 @@
 import axios from "axios";
 
-
 const employeeServicesApi = axios.create({
     baseURL: "http://localhost:8000/employee_services",
 });
 
 const establishmentServicesApi = axios.create({
     baseURL: "http://localhost:8000/establisment",
-})
+});
 
 const employeeApi = axios.create({
     baseURL: "http://localhost:8000/employee",
-})
+});
+
+const serviceApi = axios.create({
+    baseURL: "http://localhost:8000/service",
+});
+
+const review = axios.create({
+    baseURL: "http://localhost:8000/review_employee/create_review",
+});
+
+const clientApi = axios.create({
+    baseURL: "http://localhost:8000/client",
+});
 
 //=====================================================================================
 
@@ -19,19 +30,54 @@ export function getEmployeeServices(id) {
     return employeeServicesApi.get(`/employeeServicesList/${id}/`);
 }
 
-export function deleteEmployeeService(employeeId, serviceId){
-    return employeeServicesApi.delete(`/employeeServiceDelete/${employeeId}/${serviceId}/`)
+export function deleteEmployeeService(employeeId, serviceId) {
+    return employeeServicesApi.delete(
+        `/employeeServiceDelete/${employeeId}/${serviceId}/`
+    );
 }
 
-export function addEmployeeService(employeeId, serviceId, establismentId){
-    return employeeApi.post(`/addservice/${employeeId}/`,{
+export function addEmployeeService(employeeId, serviceId, establismentId) {
+    return employeeApi.post(`/addservice/${employeeId}/`, {
         service_id: serviceId,
-        establisment_id: establismentId
-    })
+        establisment_id: establismentId,
+    });
+}
+
+export function getService(id) {
+    return serviceApi.get(`/get_service/${id}/`);
 }
 
 //=====================================================================================
 
 export function getEstablishmentServices(id) {
     return establishmentServicesApi.get(`/servicesByEstablisment/${id}/`);
+}
+
+//=====================================================================================
+
+export function getClient(id) {
+    return clientApi.get(`/get_client/${id}/`);
+}
+
+export function editClient(id, first_name, last_name, phone) {
+    return clientApi.put(`/update_client/${id}/`, {
+        first_name: first_name,
+        last_name: last_name,
+        phone: phone,
+    });
+}
+
+//=====================================================================================
+
+export function createReview(
+    client_id,
+    employee_id,
+    appointment_id,
+    comment,
+    rating
+) {
+    return review.post(`/${client_id}/${employee_id}/${appointment_id}/`, {
+        comment: comment,
+        rating: rating,
+    });
 }
--- client/src/components/client/ClientHistorial.jsx
+++ client/src/components/client/ClientHistorial.jsx
@@ -3,16 +3,35 @@ import ClientPerfil from "./ClientPerfil"
 import ClientService from "./ClientService"
 import { useEffect, useState } from "react"
 import { getClientHistory, getClientReviews } from "@/Api/profile/profileApi"
+import { getClient } from "@/Api/employeeServices/employeeServicesApi"
 
 function ClientHistorial() {
 
     const [clientHistory, setClientHistory] = useState([]);
     const [reviews, setReviews] = useState([]);
+    const [client, setClient] = useState({});
 
     useEffect(() => {
+        function loadClient() {
+            const promise = new Promise((resolve, reject) => {
+                const response = getClient(1);
+                setTimeout(() => {
+                    resolve(response);
+                    reject("Ocurrio un error");
+                }, 1000);
+            });
+            promise.then((response) => {
+                setClient(response.data);
+                console.log(response);
+            });
+            promise.catch((error) => {
+                console.log(error);
+            })
+        }
+
         function loadClientHistory() {
             const promise = new Promise((resolve, reject) => {
-                const response = getClientHistory(1);
+                const response = getClientHistory(client.id);
                 setTimeout(() => {
                     resolve(response);
                     reject("Ocurrio un error");
@@ -26,31 +45,34 @@ function ClientHistorial() {
             });
         }
 
+        
+
+        loadClient();
         loadClientHistory();
-
-        function loadClientReviews(){
-            const promise = new Promise((resolve, reject) => {
-                const response = getClientReviews(1)
-                setTimeout(() => {
-                    resolve(response)
-                    reject("Ocurrio un error")
-                }, 1000);
-            })
-            promise.then((response) => {
-                setReviews(response.data)
-            })
-            promise.catch((error) => {
-                console.log(error)
-            })
-        }
-
         loadClientReviews();
-    }, [])
+    }, [client.id,])
 
-    console.log("Historial de citas: ",clientHistory)
+    function loadClientReviews() {
+        const promise = new Promise((resolve, reject) => {
+            const response = getClientReviews(client.id)
+            setTimeout(() => {
+                resolve(response)
+                reject("Ocurrio un error")
+            }, 1000);
+        })
+        promise.then((response) => {
+            setReviews(response.data)
+        })
+        promise.catch((error) => {
+            console.log(error)
+        })
+    }
+
+    // console.log("Historial de citas: ", clientHistory)
+    // console.log("cliente: ", client)
 
     return (
-        <div className="w-full h-[93.6vh] flex flex-col md:px-64 px-52 py-10 ">
+        <div className="w-full lg:h-[93.6vh] flex flex-col 2xl:px-64 xl:px-20  py-10 transition-all duration-300">
             <div className="flex flex-row gap-4 items-center">
                 <Button isIconOnly variant="bordered" className="rounded-full border-2 border-slate-200 shadow-sm shadow-slate-200">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
@@ -59,15 +81,20 @@ function ClientHistorial() {
                 </Button>
                 <h1 className="text-4xl font-bold">Perfil e historial</h1>
             </div>
-            <div className="grid grid-cols-[0.4fr_1.6fr] h-full mt-8">
-                <ClientPerfil />
+            <div className="grid lg:grid-cols-[0.4fr_1.6fr] grid-cols-1 h-full mt-8">
+                <ClientPerfil client={client} />
                 <article className="ml-8 flex flex-col gap-4 overflow-y-scroll pr-4
                 scrollbar scrollbar-thumb-slate-200 scrollbar-thumb-rounded-full scrollbar-track-rounded-full active:scrollbar-thumb-primary hover:scrollbar-thumb-slate-300">
-                    <h2 className="text-3xl font-bold fixed z-10 bg-white w-full">Historial</h2>
-                    <div className="h-full mt-10 gap-4 flex flex-col">
-                        {clientHistory?.map((service) => (
-                            <ClientService key={service.id} service={service} reviews={reviews} />
-                        ))}
+                    <h2 className="text-3xl font-bold lg:fixed z-10 bg-white w-full">Historial</h2>
+                    <div className="lg:h-full mt-10 gap-4 flex flex-col">
+                        {clientHistory.length > 0 ? clientHistory?.map((service) => (
+                            <ClientService key={service.id} appointments={service} reviews={reviews} loadClientReviews={loadClientReviews} />
+                        ))
+                            :
+                            <div className="flex flex-col gap-4 justify-center w-full items-center">
+                                <h2 className="text-3xl font-bold">No hay nada para ver aqui</h2>
+                            </div>
+                        }
                     </div>
                 </article>
             </div>
