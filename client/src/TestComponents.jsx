c368006352f59fc72bdbb1b92d8d0d612bacda50
Kevin Felipe Bolaños Orozco <kevinboor5725@gmail.com>
2024-12-10 21:02:25 -0500

    Replace button and authentication logic with RangeCalendar component in TestComponents

--- client/src/TestComponents.jsx
+++ client/src/TestComponents.jsx
@@ -1,21 +1,11 @@
-import { Button } from "@nextui-org/react";
-import useAuthStore from "./stores/useAuthStore";
-import AuthModal from "./components/auth/AuthModal";
-import Cookies from "js-cookie";
-function TestComponents() {
-  const { isAuthenticated, triggerAuthModal } = useAuthStore();
+import { RangeCalendar } from "@nextui-org/react";
+import { useState } from "react";
 
-  const handleProtectedAction = () => {
-    if (!Cookies.get("isAuthenticated")) {
-      triggerAuthModal();
-    } else {
-      console.log("Protected action executed");
-    }
-  };
+function TestComponents() {
+
   return (
-    <main className="grid place-content-center gap-4 h-screen">
-      <Button color="primary" onClick={handleProtectedAction}>Reservar cita</Button>
-      <AuthModal />
+    <main>
+      <RangeCalendar className="w-full" />
     </main>
   );
 }
