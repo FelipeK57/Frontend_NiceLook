3b22760ec06fb2ce2f0c0e13a064f1ccc440302f
Kevin Felipe Bolaños Orozco <kevinboor5725@gmail.com>
2024-12-12 10:47:34 -0500

    Cambios en la interfaz con el fin de mantener un diseño responsivo en la pagina de Gestion de horarios y disponibilidad

--- client/src/pages/employee/TimesManagement.jsx
+++ client/src/pages/employee/TimesManagement.jsx
@@ -5,31 +5,25 @@ import { useEffect, useState } from "react";
 function TimesManagement() {
   const date = new Date().getMonth();
   const [month, setMonth] = useState(date);
-  console.log(date)
+  console.log(date);
 
   return (
-    <main className="flex flex-col gap-6 min-h-screen py-4 px-6">
-      <header>
+    <main className="grid grid-rows-[auto_1fr_auto] gap-4 py-2 px-6">
+      <header className="flex flex-col gap-4 md:flex-row justify-between md:items-center">
         <h1 className="text-2xl lg:text-4xl text-slate-950 font-bold w-full">
           Gestión de agenda y disponibilidad
         </h1>
+        <MonthSelector month={date} setMonth={setMonth} />
       </header>
-      <section className="flex flex-col gap-6">
-        <div className="flex flex-row gap-6 flex-grow justify-start">
-          <ButtonCustom primary name="Agregar horario" />
-          <ButtonCustom
-            color="danger"
-            variant="light"
-            name="Agregar excepción"
-          />
-        </div>
-        <div className="flex flex-row items-center flex-grow justify-start gap-6">
-          <h2 className="text-2xl text-slate-950 font-semibold">
-            Calendario de este mes
-          </h2>
-          <MonthSelector month={date} setMonth={setMonth} />
-        </div>
+      <section className="flex flex-col gap-2">
+        <p className="text-lg lg:text-2xl font-semibold">Calendario del mes</p>
         <Calendar month2={month} />
+      </section>
+      <section className="flex flex-col gap-6">
+        <div className="flex flex-row gap-6 flex-grow justify-between md:justify-start">
+          <ButtonCustom primary name="Agregar horario" />
+          <ButtonCustom variant="light" secondary name="Agregar excepción" />
+        </div>
       </section>
     </main>
   );
@@ -108,7 +102,7 @@ const Calendar = ({ month2 }) => {
             (day, index) => (
               <div
                 key={index}
-                className="flex items-center justify-center rounded-full py-2 md:py-6 font-semibold text-sm md:text-lg"
+                className="last:border-0 last:rounded-tr-2xl first:rounded-tl-2xl flex items-center justify-center border-r-1 bg-slate-100 border-slate-200 py-2 md:py-6 font-semibold text-sm md:text-lg"
               >
                 {day}
               </div>
@@ -123,7 +117,7 @@ const Calendar = ({ month2 }) => {
             {week.map((day, index) => (
               <div
                 key={index}
-                className={`px-2 py-2 md:py-6 my-2 rounded-full font-medium text-medium text-center ${
+                className={`last:border-r-0 border-r-1 px-2 py-2 md:py-6 2xl:py-10 border-t-1 font-medium text-medium text-center ${
                   day === null ? "text-gray-400" : "text-black"
                 }`}
               >
